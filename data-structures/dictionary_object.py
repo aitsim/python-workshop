32db55d404e6ab8b0d3a1ce7af097628e88a9bdf
aitsim <aitsihamou.med@gmail.com>
2020-01-29 11:48:00 +0100

    python list object

--- data-structures/dictionary_object.py
+++ data-structures/dictionary_object.py
@@ -1,7 +1,27 @@
+# Python Collections (Arrays)
+# There are four collection data types in the Python programming language:
+#
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered and unindexed. No duplicate members.
+# Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+# When choosing a collection type, it is useful to understand the properties of that type.
+# Choosing the right type for a particular data set could mean retention of meaning,
+# and, it could mean an increase in efficiency or security.
 
 
+
+# https://www.w3schools.com/python/python_dictionaries.asp
 # dict   d={}
 # list   l=[]
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
 
 l = {}
 
