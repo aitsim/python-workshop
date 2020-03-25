2ac7aa60a5a2a336f765a6d38bf8f530dd8a6217
aitsim <aitsihamou.med@gmail.com>
2020-03-25 18:13:48 +0000

    added some unit tests

---
+++ testing/calc.py
@@ -0,0 +1,16 @@
+
+
+def add(x,y):
+    return x+y 
+
+def substract(x,y):
+    return x-y
+
+def devide(x,y):
+    if y == 0 :
+        raise ValueError("cant devide by zero")
+    return x/y
+
+def multiply(x,y):
+    return x*y
+
